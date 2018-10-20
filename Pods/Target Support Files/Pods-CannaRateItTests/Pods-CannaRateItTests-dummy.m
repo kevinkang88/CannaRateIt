2bb5bda62ccbea9644a0f874dc4077a19b58a5e2
Dong Kang <kevinkang88@gmail.com>
2018-10-20 16:44:33 -0400

    remove cocoapods

--- Pods/Target Support Files/Pods-CannaRateItTests/Pods-CannaRateItTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CannaRateItTests : NSObject
-@end
-@implementation PodsDummy_Pods_CannaRateItTests
-@end
--- Pods/Target Support Files/Pods-CannaRateItUITests/Pods-CannaRateItUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CannaRateItUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CannaRateItUITests
-@end
