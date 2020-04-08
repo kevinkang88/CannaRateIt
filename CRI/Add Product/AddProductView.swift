071456b9ded955a343dd15ace1654bec5e3965f0
Dong Kang <kevinkang88@gmail.com>
2020-04-08 08:01:33 -0400

    user is able to add image

--- CRI/Add Product/AddProductView.swift
+++ CRI/Add Product/AddProductView.swift
@@ -12,48 +12,81 @@ struct AddProductView: View {
 	
 	@ObservedObject var addProductFormDataStore = AddProductFormDataStore()
 	
+	@State var showImagePicker = false
+	@State private var image : Image? = nil
+	
     var body: some View {
 		NavigationView {
-			VStack(alignment: .leading) {
-				
-				Text("Product name").padding(.top)
-				TextField("Name", text: self.$addProductFormDataStore.productName, onEditingChanged: { changed in
-					print(changed)
-				}) {
-					print("commit yo man")
-				}.textFieldStyle(RoundedBorderTextFieldStyle())
-				
-				
-				Text("Brand").padding(.top)
-				TextField("Brand", text: self.$addProductFormDataStore.brandName, onEditingChanged: { changed in
-					print(changed)
-				}) {
-					print("commit yo man")
-				}.textFieldStyle(RoundedBorderTextFieldStyle())
-				
-				Text("Category").padding(.top)
-				CategoryPickerView(selectedCategory: self.$addProductFormDataStore.selectedCategory)
-				
-				Stepper("Rating: \(String(format: "%.1f", self.addProductFormDataStore.rating))", onIncrement: {
-					if self.addProductFormDataStore.rating <= 4.5 {
-						self.addProductFormDataStore.rating += 0.5
-					}
-				}, onDecrement: {
-					if self.addProductFormDataStore.rating >= 0.5 {
-						self.addProductFormDataStore.rating -= 0.5
-					}
-				}).padding(.top)
-				// number ticker that goes up in 0.5 increments
+			VStack {
 				
 				Button(action: {
-					print("submittingto the firebase")
+					self.showImagePicker = true
 				}) {
-					Text("create")
+					if self.image == nil {
+						Text("+ Image")
+						.fontWeight(.regular)
+						.font(.headline)
+						.foregroundColor(Color.gray)
+						.padding()
+						.overlay(
+							RoundedRectangle(cornerRadius: 20)
+								.stroke(Color.gray, lineWidth: 3))
+					} else {
+						Text("Added")
+						.fontWeight(.regular)
+						.font(.headline)
+						.foregroundColor(Color.gray)
+						.padding()
+						.overlay(
+							RoundedRectangle(cornerRadius: 20)
+								.stroke(Color.gray, lineWidth: 3))
+					}
+
 				}.padding(.top)
-				Spacer()
+				
+				VStack(alignment: .leading) {
+					Text("Product name").padding(.top)
+					TextField("Name", text: self.$addProductFormDataStore.productName, onEditingChanged: { changed in
+						print(changed)
+					}) {
+						print("commit yo man")
+					}.textFieldStyle(RoundedBorderTextFieldStyle())
+
+					Text("Brand").padding(.top)
+					TextField("Brand", text: self.$addProductFormDataStore.brandName, onEditingChanged: { changed in
+						print(changed)
+					}) {
+						print("commit yo man")
+					}.textFieldStyle(RoundedBorderTextFieldStyle())
+					
+					Text("Category").padding(.top)
+					CategoryPickerView(selectedCategory: self.$addProductFormDataStore.selectedCategory)
+					
+					Stepper("Rating: \(String(format: "%.1f", self.addProductFormDataStore.rating))", onIncrement: {
+						if self.addProductFormDataStore.rating <= 4.5 {
+							self.addProductFormDataStore.rating += 0.5
+						}
+					}, onDecrement: {
+						if self.addProductFormDataStore.rating >= 0.5 {
+							self.addProductFormDataStore.rating -= 0.5
+						}
+					}).padding(.top)
+					
+					Button(action: {
+						print("submittingto the firebase")
+					}) {
+						Text("create")
+					}.padding(.top)
+					
+					Spacer()
+				}
+				
+				
 			}.padding(.horizontal)
 			
 			.navigationBarTitle("Add Product")
+		}.sheet(isPresented: self.$showImagePicker) {
+			PhotoCaptureView(showImagePicker: self.$showImagePicker, image: self.$image)
 		}
         
     }
---
+++ CRI/Add Product/ImagePicker.swift
@@ -0,0 +1,69 @@
+//
+//  ImagePicker.swift
+//  CRI
+//
+//  Created by Dong Kang on 4/8/20.
+//  Copyright © 2020 Yooniverse. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker : UIViewControllerRepresentable {
+    
+    @Binding var isShown    : Bool
+    @Binding var image      : Image?
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    func makeCoordinator() -> ImagePickerCoordinator {
+        return ImagePickerCoordinator(isShown: $isShown, image: $image)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+}
+
+class ImagePickerCoordinator : NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    @Binding var isShown    : Bool
+    @Binding var image      : Image?
+    
+    init(isShown : Binding<Bool>, image: Binding<Image?>) {
+        _isShown = isShown
+        _image   = image
+    }
+    
+    //Selected Image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let uiImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        image = Image(uiImage: uiImage)
+        isShown = false
+    }
+    
+    //Image selection got cancel
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+}
+
+struct PhotoCaptureView: View {
+    
+    @Binding var showImagePicker    : Bool
+    @Binding var image              : Image?
+    
+    var body: some View {
+        ImagePicker(isShown: $showImagePicker, image: $image)
+    }
+}
+
+struct PhotoCaptureView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCaptureView(showImagePicker: .constant(false), image: .constant(Image("")))
+    }
+}
