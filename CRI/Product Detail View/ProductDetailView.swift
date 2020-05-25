62c9b1adccd2dc4f254d69594ca77cb76b7f0e30
Dong Kang <kevinkang88@gmail.com>
2020-05-25 18:53:20 -0400

    user should be able to see product details

--- CRI/Product Detail View/ProductDetailView.swift
+++ CRI/Product Detail View/ProductDetailView.swift
@@ -19,22 +19,65 @@ struct ProductDetailView: View {
 	}
 	
     var body: some View {
-		VStack {
-			ZStack(alignment: .topLeading) {
-				ImageView(withURL: "products/\(product.id ?? "").jpeg").frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 1.8, alignment: .center)
-							.edgesIgnoringSafeArea(.top)
-				
-				Button(action: {
-					 self.presentation.wrappedValue.dismiss()
-				}) {
-					Image("back-icon").resizable().frame(width: 50, height: 50, alignment: .center)
+		ZStack(alignment: .top) {
+			VStack {
+				ZStack(alignment: .topLeading) {
+					ImageView(withURL: "products/\(product.id ?? "").jpeg").frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 1.8, alignment: .center)
+								.edgesIgnoringSafeArea(.top)
+					
+					Button(action: {
+						 self.presentation.wrappedValue.dismiss()
+					}) {
+						Image("back-icon").renderingMode(.template).resizable().frame(width: 50, height: 50, alignment: .center).foregroundColor(Color.black)
+					}.offset(y: 30.0)
 				}
+				
+				Spacer()
 			}
 			
-			Spacer()
+			
+			ScrollView {
+				
+				Spacer().frame(height: 20.0)
+				
+				HStack {
+					Text(product.name).font(Font.custom("AirbnbCerealApp-Medium", size: 20.0))
+						.foregroundColor(Color.black).padding(.leading).padding(.top)
+					Spacer()
+				}
+				
+				HStack {
+					Text(product.brand).font(Font.custom("AirbnbCerealApp-Light", size: 16.0))
+						.foregroundColor(Color.gray).padding(.leading)
+					
+					Image("approval-icon").renderingMode(.template).resizable().aspectRatio(contentMode: .fit).frame(width: 20, height: 20).foregroundColor(Color.green)
+					Spacer()
+				}
+				
+				Divider().padding()
+				
+				HStack {
+					HStack {
+						Image("star").resizable().renderingMode(.template).frame(width: 16, height: 16, alignment: .center).foregroundColor(.white)
+						Text("4.0").font(Font.custom("AirbnbCerealApp-Medium", size: 14.0)).foregroundColor(Color.white)
+					}.padding(.all, 8.0).background(Color.orange).cornerRadius(10.0, corners: .allCorners)
+					
+					Spacer()
+				}.padding(.leading)
+				
+				HStack {
+					Text("Reviews").font(Font.custom("AirbnbCerealApp-Medium", size: 24.0)).foregroundColor(Color.black).padding()
+					Spacer()
+				}
+				
+				
+				Spacer()
+			}.background(Color.white).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2).cornerRadius(30.0, corners: .allCorners).offset(y: UIScreen.main.bounds.height / 2.5)
+			
+			
 		}.navigationBarTitle("") //this must be empty
 		.navigationBarHidden(true)
-		.navigationBarBackButtonHidden(true)
+			.navigationBarBackButtonHidden(true).frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
 		
-    }
+	}
 }
